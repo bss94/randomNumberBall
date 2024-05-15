473278db8f5eb73f11bcaeb7fd45c50551a204f6
Sergey Brazhnikov <serjak.2012@gmail.com>
2024-05-15 23:32:54 +0600

    edit NumBall tsx & css, create state in App.tsx, render NumBall on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-
+import {useState} from "react";
 import './App.css'
+import NumBall from "./components/NumBall/NumBall";
 
 function App() {
-
+  const [numbers, setNumbers] = useState<number[]>([5, 11, 16, 23, 32])
 
   return (
-    <>
+      <>
+        <button className='num-btn' onClick={}>New numbers</button>
+        <div className="num-ball">
+          {numbers.map((number, index) => {
+            return <NumBall number={number} key={index + number}/>
+          })}
+        </div>
 
-    </>
+      </>
   )
 }
 
--- src/components/NumBall/NumBall.tsx
+++ src/components/NumBall/NumBall.tsx
@@ -1,7 +1,13 @@
-const NumBall = () => {
+import 'NumBall.css'
+interface Props {
+    number: number;
+}
+const NumBall = (props:Props) => {
     return (
         <div>
-
+            <div className='ball'>
+                <h1 className='num'>{props.number}</h1>
+            </div>
         </div>
     );
 };
