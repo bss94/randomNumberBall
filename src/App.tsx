ad3317d27a71c6da159bd65fc17edf00870c49a0
Sergey Brazhnikov <serjak.2012@gmail.com>
2024-05-17 01:01:43 +0600

    add .editorconfig, edit eslintrc, edit styles code on work file

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,42 @@
-import {useState} from "react";
-import './App.css'
-import NumBall from "./components/NumBall/NumBall";
+import {useState} from 'react';
+import './App.css';
+import NumBall from './components/NumBall/NumBall';
 
 function App() {
-    const [numbers, setNumbers] = useState<number[]>([5, 11, 16, 23, 32]);
+  const [numbers, setNumbers] = useState<number[]>([5, 11, 16, 23, 32]);
 
-    const getRandNumbers = () => {
-        const newNumbers: number[] = [];
-        while (newNumbers.length < numbers.length) {
-            const newNumber: number = Math.floor(Math.random() * (37 - 5) + 5);
-            let check: boolean = false;
-            if (newNumbers.length > 0) {
-                for (let i = 0; i < newNumbers.length; i++) {
-                    if (newNumber === newNumbers[i]) {
-                        check = true;
-                        break;
-                    }
-                }
-            }
-            if (!check) {
-                newNumbers.push(newNumber);
-            }
+  const getRandNumbers = (): void => {
+    const newNumbers: number[] = [];
+    while (newNumbers.length < numbers.length) {
+      const newNumber: number = Math.floor(Math.random() * (37 - 5) + 5);
+      let check: boolean = false;
+      if (newNumbers.length > 0) {
+        for (let i: number = 0; i < newNumbers.length; i++) {
+          if (newNumber === newNumbers[i]) {
+            check = true;
+            break;
+          }
         }
-        setNumbers(newNumbers.sort((a, b) => {
-            return a - b;
-        }));
+      }
+      if (!check) {
+        newNumbers.push(newNumber);
+      }
     }
-    return (
-        <>
-            <button className='num-btn' onClick={getRandNumbers}>New numbers</button>
-            <div className="num-ball">
-                {numbers.map((number, index) => {
-                    return <NumBall number={number} key={index + number}/>
-                })}
-            </div>
-        </>
-    )
+    setNumbers(newNumbers.sort((a, b) => {
+      return a - b;
+    }));
+  };
+
+  return (
+    <>
+      <button className="num-btn" onClick={getRandNumbers}>New numbers</button>
+      <div className="num-ball">
+        {numbers.map((number: number, index: number) => {
+          return <NumBall number={number} key={index + number}/>;
+        })}
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/components/NumBall/NumBall.tsx
+++ src/components/NumBall/NumBall.tsx
@@ -1,17 +1,17 @@
-import './NumBall.css'
+import './NumBall.css';
 
 interface Props {
-    number: number;
+  number: number;
 }
 
 const NumBall = (props: Props) => {
-    return (
-        <div>
-            <div className='ball'>
-                <h1 className='num'>{props.number}</h1>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="ball">
+        <h1 className="num">{props.number}</h1>
+      </div>
+    </div>
+  );
 };
 
 export default NumBall;
